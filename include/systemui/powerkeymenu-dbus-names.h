bec784d7e73eb54c91be0e3046a5b0d27fc6f457
Pali Rohár <pali.rohar@gmail.com>
2012-12-14 18:20:23 +0100

    Add new package osso-systemui-powerkeymenu-dev with header file powerkeymenu-dbus-names.h

---
+++ include/systemui/powerkeymenu-dbus-names.h
@@ -0,0 +1,37 @@
+/**
+ * @file powerkeymenu-dbus-names.h
+ * DBus Interface to the System UI Powerkeymenu plugin
+ * <p>
+ * This file is part of osso-systemui-powerkeymenu
+ * <p>
+ * Copyright (C) 2012 Pali Rohár <pali.rohar@gmail.com>
+ *
+ * These headers are free software; you can redistribute them
+ * and/or modify them under the terms of the GNU Lesser General Public
+ * License version 2.1 as published by the Free Software Foundation.
+ *
+ * These headers are distributed in the hope that they will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA
+ */
+
+#ifndef _SYSTEMUI_POWERKEYMENU_DBUS_NAMES_H
+#define _SYSTEMUI_POWERKEYMENU_DBUS_NAMES_H
+
+/**
+ * Request to open power key menu.
+ */
+#define SYSTEMUI_POWERKEYMENU_OPEN_REQ     "powerkeymenu_open"
+
+/**
+ * Request to close power key menu.
+ */
+#define SYSTEMUI_POWERKEYMENU_CLOSE_REQ    "powerkeymenu_close"
+
+#endif
